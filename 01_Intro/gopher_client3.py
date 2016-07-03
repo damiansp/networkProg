74fe0de9319e70f743f35b5f7033b95cfc9a5c4c
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-07-03 19:07:34 -0400

    Simple Gopher client with file-like interface

---
+++ 01_Intro/gopher_client3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# Simple Gopher Client with basic error handling
+import socket, sys
+
+port = 70 # port used by Gopher
+host = sys.argv[1]
+filename = sys.argv[2]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+fd = s.makefile('rw', 0)
+fd.write(filename + '\r\n')
+
+for line in fd.readlines():
+    sys.stdout.write(line)
+
+
+
+# To run
+# > ./gopher_client3.py url.com /
+    
