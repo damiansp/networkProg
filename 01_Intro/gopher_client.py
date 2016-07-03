f2c20747d039884b59e4a389c45df548a9360a27
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-07-03 18:48:24 -0400

    Very basic server

---
+++ 01_Intro/gopher_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# Simple Gopher Client
+import socket, sys
+
+port = 70 # port used by Gopher
+host = sys.argv[1]
+filename = sys.argv[2]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+s.sendall(filename + '\r\n')
+
+while True:
+    buf = s.recv(2048)
+    if not len(buf):
+        break
+    sys.stdout.write(buf)
+
+
+
+# To run
+# > ./gopher_client.py url.com /
+    
