c7fc969f1e3e191975960e8bc85f6862402a996a
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-07-06 14:13:44 -0400

    Basic server with error handling

---
+++ 03_Network_Servers/error_handling_server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# Server with error handling
+import socket, traceback
+
+host = ''
+port = 54321
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((host, port))
+s.listen(1)
+
+while True:
+    try:
+        clientsock, clientaddr = s.accept()
+    except KeyboardInterrupt:
+        raise
+    except:
+        traceback.print_exc()
+        continue
+
+    # Process connection
+    try:
+        print "Received connection from", clientsock.getpeername()
+        # ...Process here...
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except:
+        traceback.print_exc()
+
+    # Close connection
+    try:
+        clientsock.close()
+    except KeyboardInterrupt:
+        raise
+    except:
+        traceback.print_exc()
